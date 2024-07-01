91016651953c992164e89d8778d0e7b2595e1467
Daram0o0 <c30728535@gmail.com>
2024-07-01 23:46:20 +0900

    add files

---
+++ hover-animation/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import EMB from "./EMB.png";
+
+function App() {
+  return (
+    <div>
+      <main>
+        <h1 className="text">EMBARRASSMENT</h1>
+        <h1 className="stroke">EMBARRASSMENT</h1>
+        <img src={EMB} className="image" alt="embarrassement" />
+      </main>
+    </div>
+  );
+}
+
+export default App;
